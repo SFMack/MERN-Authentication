3988c327ad3f05fcda90077193f92b14b0c5a272
SFMack <sfcamack@icloud.com>
2019-07-21 01:00:19 -0700

    update

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://christian:Loudpaq7139@rime-aibhm.mongodb.net/test?retryWrites=true&w=majority'
-}
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+const config = require('config');
 
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
