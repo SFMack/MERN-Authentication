dba8ec8bd3681cc2fbe46b7913b2b2b684907954
SFMack <sfcamack@icloud.com>
2019-07-20 23:14:52 -0700

    update

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: `mongodb+srv://christian:Loudpaq7139@rime-aibhm.mongodb.net/test?retryWrites=true&w=majority`
+    mongoURI: 'mongodb+srv://christian:<password>@rime-aibhm.mongodb.net/test?retryWrites=true&w=majority'
 }
